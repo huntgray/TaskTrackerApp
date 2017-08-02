76299d33b1a88080c2382d346116685e18e6d81b
Benjamin Hunt Gray <benjamin.gray@yahoo.com>
2017-08-02 01:53:24 -0400

    removes external JSON object and getsInitialState of todo_items; populates UL from this.state

--- src/app/index.js
+++ src/app/index.js
@@ -3,23 +3,24 @@ var ReactDOM = require('react-dom');
 
 //Create component
 var ToDoComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      todo_items: ['Wash the car', 'Buy groceries', 'Cut the grass']
+    }
+  },
   render: function(){
     return(
-      <div>
+      <div id="todo-list">
         <h1>TaskTrackerApp</h1>
         <ul>
-          <li>{todo_items.items[0]}</li>
-          <li>{todo_items.items[1]}</li>
-          <li>{todo_items.items[2]}</li>
+          <li>{this.state.todo_items[0]}</li>
+          <li>{this.state.todo_items[1]}</li>
+          <li>{this.state.todo_items[2]}</li>
         </ul>
       </div>
     )
   }
 });
 
-var todo_items = {
-  items: ['Wash the car', 'Buy groceries', 'Cut the grass']
-}
-
 //Places component in index.html
-ReactDOM.render(<ToDoComponent todo_items={todo_items} />, document.getElementById('todo-wrapper'));
+ReactDOM.render(<ToDoComponent />, document.getElementById('todo-wrapper'));
