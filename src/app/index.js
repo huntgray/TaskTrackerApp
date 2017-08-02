de81c97bf6ad9a82482a5fbd5ba5f855c0733532
Benjamin Hunt Gray <benjamin.gray@yahoo.com>
2017-08-02 02:16:59 -0400

    sets state in ToDoComponent with setTimeout function call after 5 seconds

--- src/app/index.js
+++ src/app/index.js
@@ -4,14 +4,27 @@ var ReactDOM = require('react-dom');
 //Create component
 var ToDoComponent = React.createClass({
   getInitialState: function(){
+    var todo_items = ['Wash the car', 'Buy groceries', 'Cut the grass'],
+    todo_items_length = todo_items.length;
     return {
-      todo_items: ['Wash the car', 'Buy groceries', 'Cut the grass']
+      todo_items: todo_items,
+      num_items: todo_items_length
     }
   },
   render: function(){
+    //setState and bind this obect to the following function
+    var adjustLength = setTimeout(function(){
+
+      this.setState({
+        num_items: (function(){
+          return 100;
+        }())
+      });
+    }.bind(this), 5000);
     return(
       <div id="todo-list">
         <h1>TaskTrackerApp</h1>
+        <p><strong># of Items To Do: </strong>{this.state.num_items}</p>
         <ul>
           <li>{this.state.todo_items[0]}</li>
           <li>{this.state.todo_items[1]}</li>
