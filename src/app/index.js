2fe1923cd2f47336a9e0f1199c76b25eb2e8b928
Benjamin Hunt Gray <benjamin.gray@yahoo.com>
2017-08-02 01:22:01 -0400

    adds first React component; places in index

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log('server running on port 1234');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Create component
+var ToDoComponent = React.createClass({
+  render: function(){
+    return(
+      <h1>TaskTrackerApp</h1>
+    )
+  }
+});
+
+//Places component in index.html
+ReactDOM.render(<ToDoComponent />, document.getElementById('todo-wrapper'));
